d802c3c182bcf0438df6c0d4236f77a39b27dbec
Andreas von Haaren <andreasvonhaaren99@gmail.com>
2022-01-24 15:41:27 +0000

    fix zsh plugin download

--- install-zsh.sh
+++ install-zsh.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
-sudo apt update && sudo apt upgrade
-sudo apt install zsh
+sudo apt update && sudo apt --yes upgrade
+echo "Installing zsh and recommended plugins"
+sudo apt --yes install zsh git
 chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:~/.oh-my-zsh/custom}/themes/powerlevel10k
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" "" --unattended
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
-cp dotfiles/.p10k.zsh ~
-cp dotfiles/.zshenv ~
-cp dotfiles/.zshrc ~
+echo "Setting up basic configuration"
+cp wsl-install/dotfiles/.p10k.zsh ~
+cp wsl-install/dotfiles/.zshenv ~
+cp wsl-install/dotfiles/.zshrc ~
+
+echo "Cleaning up"
+sudo rm -rf wsl-install
+
+echo "Please restart Ubuntu by executing 'wsl --shutdown' and reopening"
